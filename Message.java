83897abf401b6e66b3bfa44a0bfc0234e5cc01e0
reidcastillo <116670837+reidcastillo@users.noreply.github.com>
2026-03-27 10:59:52 -0400

    Update Message.java
    fixed comments

--- Message.java
+++ Message.java
@@ -2,6 +2,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+// Sealed interface which shows all possible message types exchanged  between client & server
 public sealed interface Message {
 	// Write a message to a DataOutputStream.
 	public void send(DataOutputStream dout) throws IOException;
@@ -22,19 +23,20 @@ public sealed interface Message {
 		}
 	}
 
+	// sends simple strings 
 	public record TextMessage(String msg) implements Message {
 		static final byte code = 0;
-
+// Sends the message type
 		public void send(DataOutputStream dout) throws IOException {
 			dout.write(code);
 			dout.writeUTF(msg);
 		}
-
+// Reads string from input stream and then mnakes a TextMessage
 		public static Message recv(DataInputStream din) throws IOException {
 			return new TextMessage(din.readUTF());
 		}
 	}
-
+// Used for error responses 
 	public record ErrorMessage(String msg) implements Message {
 		static final byte code = 1;
 
@@ -42,12 +44,12 @@ public sealed interface Message {
 			dout.write(code);
 			dout.writeUTF(msg);
 		}
-
+// Reads error string then makes the message
 		public static Message recv(DataInputStream din) throws IOException {
 			return new ErrorMessage(din.readUTF());
 		}
 	}
-
+// Used for sending file data as raw bytes
 	public record BinaryMessage(byte[] data) implements Message {
 		static final byte code = 2;
 
@@ -56,7 +58,7 @@ public sealed interface Message {
 			dout.writeInt(data.length);
 			dout.write(data);
 		}
-
+// Reads binary data
 		public static Message recv(DataInputStream din) throws IOException {
 			int length = din.readInt();
 			byte[] data = new byte[length];
